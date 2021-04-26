bbbf34246d1b4504f8fb733a2cbd29044ffa666e
student <o1083012@gm.pu.edu.tw>
2021-04-26 14:24:41 +0800

    04/26

--- app/src/main/java/tw/edu/pu/o1083012/example/handshape/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1083012/example/handshape/MainActivity.kt
@@ -14,6 +14,7 @@ import tw.edu.pu.o1083012.example.handshape.ml.Shapes
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         btn.setOnClickListener(object : View.OnClickListener {
